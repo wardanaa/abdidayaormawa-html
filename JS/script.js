5826ca0f51122e8fd7d4dbfd199fd26444327080
dewamardana <dodekmardana33@gmail.com>
2024-09-17 00:48:24 +0800

    Memperbaiki Navbar dan menghubungkan hampir semua tag a dan merapikan halaman

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var openModalButton = document.getElementById('open-modal')
+    var closeModalButton = document.getElementById('close-modal')
+    var modal = document.getElementById('audio-modal')
+    var audio = document.getElementById('modal-audio')
+
+    console.log('openModalButton:', openModalButton)
+    console.log('closeModalButton:', closeModalButton)
+    console.log('modal:', modal)
+    console.log('audio:', audio)
+
+    if (!openModalButton || !closeModalButton || !modal || !audio) {
+        console.error('One or more elements are not found.')
+        return
+    }
+
+    // Show modal with animation
+    openModalButton.addEventListener('click', function () {
+        console.log('Open button clicked')
+        modal.classList.add('opacity-100') // Tambahkan kelas opacity-100 untuk menampilkan modal
+        modal.querySelector('div').classList.add('scale-100')
+        audio.play() // Pastikan audio mulai bermain ketika modal terbuka
+    })
+
+    // Hide modal with animation and stop audio
+    closeModalButton.addEventListener('click', function () {
+        console.log('Close button clicked')
+        modal.classList.remove('opacity-100')
+        modal.querySelector('div').classList.remove('scale-100')
+        setTimeout(function () {
+            modal.classList.remove('flex') // Hapus kelas flex setelah animasi selesai
+        }, 300)
+        audio.pause()
+        audio.currentTime = 0
+    })
+
+    // Hide modal when clicking outside and stop audio
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            console.log('Clicked outside modal')
+            modal.classList.remove('opacity-100')
+            modal.querySelector('div').classList.remove('scale-100')
+            setTimeout(function () {
+                modal.classList.remove('flex') // Hapus kelas flex setelah animasi selesai
+            }, 300)
+            audio.pause()
+            audio.currentTime = 0
+        }
+    })
+})
+
 // JavaScript untuk mendeteksi scroll dan mengubah background navbar
 window.addEventListener('scroll', function () {
     const navbar = document.getElementById('navbar')
